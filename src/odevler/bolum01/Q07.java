97e3aaaf7d3736790c05165bb646d42e9d7b8b62
Kaptanq <berkay67830@gmail.com>
2024-08-17 16:39:46 +0300

    Update

---
+++ src/odevler/bolum01/Q07.java
@@ -0,0 +1,10 @@
+package odevler.bolum01;
+
+public class Q07 {
+    public static void main(String[] args) {
+        double result1= 4*(1.0 - 1.0/3 + 1.0/5 - 1.7 + 1.9 - 1.0/11);
+        double result2= result1 + 1.0/13;
+        System.out.println("Approximation1: "+result1);
+        System.out.println("Approximation2: "+result2);
+    }
+}
